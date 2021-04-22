0437370d31696d3752484bd0d89d209353e4ca4c
Doğu Kaan Çalışkan <63837864+dotju@users.noreply.github.com>
2021-04-22 04:19:42 +0300

    Update and rename Vowel Count - Python to Vowel Count.py

---
+++ 7 - Kyu/Vowel Count.py
@@ -0,0 +1,20 @@
+
+# Return the number (count) of vowels in the given string.
+
+# We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+# The input string will only consist of lower case letters and/or spaces
+
+
+# link = https://www.codewars.com/kata/54ff3102c1bad923760001f3
+
+
+
+def get_count(input_str):
+    num_vowels = 0
+    vowels = set("aeiou")
+    for x in input_str:
+        if x in vowels:
+            num_vowels = num_vowels + 1
+    return num_vowels
+
