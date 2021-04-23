7eac4872b5e8b1a4863499e9fb9a719b2ca89285
Doğu Kaan Çalışkan <63837864+dotju@users.noreply.github.com>
2021-04-24 02:14:10 +0300

    Update and rename Who likes it? - Python to Who likes it?.py

---
+++ 6 - Kyu/Who likes it?.py
@@ -0,0 +1,31 @@
+
+# You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+# Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item. It must return the display text as shown in the examples:
+
+# sample :
+# likes [] -- must be "no one likes this"
+# likes ["Peter"] -- must be "Peter likes this"
+# likes ["Jacob", "Alex"] -- must be "Jacob and Alex like this"
+# likes ["Max", "John", "Mark"] -- must be "Max, John and Mark like this"
+# likes ["Alex", "Jacob", "Mark", "Max"] -- must be "Alex, Jacob and 2 others like this"
+
+# link = https://www.codewars.com/kata/5266876b8f4bf2da9b000362
+
+
+def likes(names):
+
+    y = len(names)
+    if y == 1:
+        return f"{names[0]} likes this"
+    elif y == 2:
+        return f"{names[0]} and {names[1]} like this" 
+    elif y == 3:
+        return f"{names[0]}, {names[1]} and {names[2]} like this"
+    elif y >= 4:
+        return f"{names[0]}, {names[1]} and {y - 2} others like this"        
+    else:
+        return 'no one likes this'
+    pass
+
+
