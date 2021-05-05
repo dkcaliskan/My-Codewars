ab4e497ff52fdc715d5cba22cc32d68f4031841e
Doğu Kaan Çalışkan <63837864+dotju@users.noreply.github.com>
2021-05-05 04:45:26 +0300

    Create Basic Math (Add or Subtract).js

---
+++ 7 - Kyu/Basic Math (Add or Subtract).js
@@ -0,0 +1,16 @@
+// In this kata, you will do addition and subtraction on a given string. The return value must be also a string.
+
+// Note: the input will not be empty.
+// Examples
+
+// "1plus2plus3plus4"  --> "10"
+// "1plus2plus3minus4" -->  "2"
+
+// link = https://www.codewars.com/kata/5809b62808ad92e31b000031
+
+
+def disemvowel(string):
+    a = "aeiouAEIOU"
+    for x in a:
+        string = string.replace(x, "")
+    return string
