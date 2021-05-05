c9cdab788679f9f1e31a67acc84429b03ec81484
Doğu Kaan Çalışkan <63837864+dotju@users.noreply.github.com>
2021-05-05 04:48:07 +0300

    Create 7 kyu All Star Code Challenge #22.js

---
+++ 7 - Kyu/7 kyu All Star Code Challenge #22.js
@@ -0,0 +1,27 @@
+// This Kata is intended as a small challenge for my students
+
+// All Star Code Challenge #22
+
+// Create a function that takes an integer argument of seconds and converts the value into a string describing how many hours and minutes comprise that many seconds.
+
+// Any remaining seconds left over are ignored.
+
+// Note:
+// The string output needs to be in the specific form - "X hour(s) and X minute(s)"
+
+// For example:
+
+// 3600 --> "1 hour(s) and 0 minute(s)"
+// 3601 --> "1 hour(s) and 0 minute(s)"
+// 3500 --> "0 hour(s) and 58 minute(s)"
+// 323500 --> "89 hour(s) and 51 minute(s)"
+
+
+// link = https://www.codewars.com/kata/5865cff66b5699883f0001aa
+
+
+function toTime(seconds) {
+  let hour = parseInt(seconds / 3600)
+  let min = parseInt(((hour * 3600) - seconds) / 60)
+  return `${hour} hour(s) and ${min * - 1} minute(s)`
+}
